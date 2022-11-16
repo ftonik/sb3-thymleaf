294f9f1fb2b03cce36f559b6be3e9292337a2b12
Artsiom Osipenko <osipenko@scand.com>
2022-11-16 15:15:26 +0300

    Init

---
+++ src/test/java/com/example/sb3thymleaf/Sb3ThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb3thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3ThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
